2d0f4ed7d426b2cf5606f5a5e55d7466359ebce3
Sang Nguyen <sangnguyenplus@gmail.com>
2023-05-17 15:01:59 +0700

    Update url-redirector.php

--- resources/lang/en/url-redirector.php
+++ resources/lang/en/url-redirector.php
@@ -2,9 +2,9 @@
 
 return [
     'menu' => 'URL Redirector',
-    'original' => 'Original Url',
-    'target' => 'Target Url',
-    'create' => 'New url',
-    'edit' => 'View url',
+    'original' => 'Original URL',
+    'target' => 'Target URL',
+    'create' => 'Create a URL',
+    'edit' => 'Edit URL',
     'visits' => 'Visits',
 ];
